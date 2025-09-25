3e83e9f740f28d99d21c927b4f22d678f6f18eb7
rahul <rahulkumarhaluwai@gmail.com>
2025-09-25 10:00:19 +0545

    padding changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
         </div>
 
 
-   <div className='px-4 sm:px-20 xl:px-32'>
+   <div>
         <div className='text-center'>
           <h2 className='text-slate-700 text-[42px] font-semibold'>Powerful AI Tools</h2>
           <p className='text-gray-500 max-w-lg mx-auto'>
